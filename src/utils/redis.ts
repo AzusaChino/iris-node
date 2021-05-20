6d8fbdbffa5992fee03d2f466803b2a732552471
AzusaChino <azusa146@gmail.com>
2021-05-20 15:18:37 +0800

    add redis client

---
+++ src/utils/redis.ts
@@ -0,0 +1,20 @@
+import redis, { ClientOpts } from "redis";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: ".env",
+});
+
+const REDIS_HOST = process.env.REDIS_HOST;
+const REDIS_PORT = process.env.REDIS_HOST;
+const REDIS_PASS = process.env.REDIS_PASS;
+
+const option = {
+  host: REDIS_HOST,
+  port: REDIS_PORT,
+  password: REDIS_PASS,
+} as ClientOpts;
+
+const client = redis.createClient(option);
+
+export default client;
